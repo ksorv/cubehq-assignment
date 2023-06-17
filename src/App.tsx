df3d0f8d80fc602f29f96faf160e3346b1e2f2aa
Saurav Kumar <sauravkhdoolia@gmail.com>
2023-06-17 20:58:53 +0530

    use routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import {Navigate, Route, Routes} from 'react-router-dom'
+import Home from "./modules/home/components"
+
 function App() {
-  return (
-    <>
-      <div>
-        Yo
-      </div>
-    </>
-  )
+    return (
+        <Routes>
+            <Route id='home' path={'/customers/*'} Component={Home}/>
+            <Route id='rest' path={'*'} element={<Navigate to={'/customers'}/>}/>
+        </Routes>
+    );
 }
 
 export default App
